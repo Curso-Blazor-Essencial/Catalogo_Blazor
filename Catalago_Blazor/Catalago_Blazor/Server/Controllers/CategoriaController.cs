faf94c6c32eb570d181f0307186d00e90463e4e7
Leonardo Castilho <leonardo.castilho@viannasempre.com.br>
2022-08-17 15:23:28 -0300

    Fix
    Correção de pequeno erro de sintaxe

--- Catalago_Blazor/Catalago_Blazor/Server/Controllers/CategoriaController.cs
+++ Catalago_Blazor/Catalago_Blazor/Server/Controllers/CategoriaController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Catalago_Blazor.Server.Context;
+using Catalago_Blazor.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,36 +11,52 @@ namespace Catalago_Blazor.Server.Controllers
     [ApiController]
     public class CategoriaController : ControllerBase
     {
-        // GET: api/<CategoriaController>
+        private readonly AppDbContext context;
+
+        public CategoriaController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<List<Categoria>>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return await context.Categorias.AsNoTracking().ToListAsync();
         }
 
-        // GET api/<CategoriaController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("{id}", Name = "GetCategoria")]
+
+        public async Task<ActionResult<Categoria>> Get(int id)
         {
-            return "value";
+            return await context.Categorias.FirstOrDefaultAsync(x => x.CategoriaId == id);
         }
 
-        // POST api/<CategoriaController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<Categoria>> Post(Categoria categoria)
         {
+            context.Add(categoria);
+            await context.SaveChangesAsync();
+            return new CreatedAtRouteResult("GetCategoria", new { id = categoria.CategoriaId }, categoria);
         }
 
-        // PUT api/<CategoriaController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPut]
+        public async Task<ActionResult<Categoria>> Put(Categoria categoria)
         {
+            context.Entry(categoria).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return Ok(categoria);
         }
 
-        // DELETE api/<CategoriaController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpDelete]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Categoria>> Delete(int id)
         {
+            var categoria = new Categoria { CategoriaId = id };
+            context.Remove(categoria);
+            await context.SaveChangesAsync();
+            return Ok(categoria);
         }
+
+
     }
 }
