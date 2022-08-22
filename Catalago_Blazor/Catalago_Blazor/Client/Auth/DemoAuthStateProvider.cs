9b88ee4e4e02624e62b047e21d0cd56b595ddfc7
Leonardo Castilho <leonardo.castilho@viannasempre.com.br>
2022-08-22 11:58:45 -0300

    Implementando AuthenticationStateProvider
    Início da aplicação de autenticação e segurança em nosso aplicativo.

---
+++ Catalago_Blazor/Catalago_Blazor/Client/Auth/DemoAuthStateProvider.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Catalago_Blazor.Client.Auth
+{
+    public class DemoAuthStateProvider : AuthenticationStateProvider
+    {
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            await Task.Delay(4000);
+            //idicamos se o usuátio está autenticando e também seus claims
+            // definição de identidade anônima
+            var usuario = new ClaimsIdentity();
+
+            // entrando na aplicação como usuário anônimo
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(usuario)));
+        }
+    }
+}
--- Catalago_Blazor/Catalago_Blazor/Client/Program.cs
+++ Catalago_Blazor/Catalago_Blazor/Client/Program.cs
@@ -1,4 +1,6 @@
 using Catalago_Blazor.Client;
+using Catalago_Blazor.Client.Auth;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +9,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//Depednências do pacote Nugget
+builder.Services.AddAuthorizationCore();
+//Depedência do arquivo de Autenticação de criamos
+builder.Services.AddScoped<AuthenticationStateProvider, DemoAuthStateProvider>();
 
 await builder.Build().RunAsync();
