9968a54414b0704eb92492c1c4defb62e6f3f713
Leonardo Castilho <leonardo.castilho@viannasempre.com.br>
2022-08-22 17:10:57 -0300

    Controlador AccountController
    Implementação do controlador de geração de token JWT

---
+++ Catalago_Blazor/Catalago_Blazor/Shared/Models/UserInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catalago_Blazor.Shared.Models
+{
+    public class UserInfo
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
